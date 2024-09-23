834ca84e18945c9330be0f593643bdb3d021ca92
MikaelEsenbaev <mikaelesenbaev.dev@gmail.com>
2024-09-23 23:16:06 +0200

    working AddTwoNumbers solution

---
+++ AddTwoNumbers/Solution.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode result = new ListNode();
+        ListNode current = result;
+        int carryOver = 0;
+        while (l1 != null)
+        {
+            int addition = carryOver + l1.val + l2.val;
+            carryOver = 0;
+            if (addition > 9)
+            {
+                addition = addition - 10;
+                carryOver = 1;
+            }
+
+
+
+            current.val = addition;
+            current = current.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return result;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Solution solution = new Solution();
+
+ListNode l1 = new ListNode();
+l1.val = 2;
+l1.next.val = 4;
+l1.next.next.val = 3;
+
+ListNode l2= new ListNode();
+l2.val = 5;
+l2.next.val = 6;
+l2.next.next.val = 4;
+
